3ae0aaa7ebc7cd95ac56597752ab83989f81cfb9
Ghelder <Jhelder@outlook.com>
2023-07-07 20:16:21 -0700

    test: add more validations

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,13 +1,36 @@
 """Module for testing the BaseModel class"""
+
 import unittest
+from datetime import datetime
 from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
     """Test for the BaseModel class"""
 
-    def testOfClass(self):
-        """Test for the __init__ method"""
-        b1 = BaseModel()
-        b1.name = "My First Model"
-        self.assertEqual(b1.name, b1.name)
+    def test_init(self):
+        """Test the initialization of the BaseModel"""
+        model = BaseModel()
+        self.assertIsInstance(model.id, str)
+        self.assertIsInstance(model.created_at, datetime)
+        self.assertIsInstance(model.updated_at, datetime)
+
+    def test_str(self):
+        """Test the string representation of the BaseModel"""
+        model = BaseModel()
+        self.assertIsInstance(str(model), str)
+
+    def test_save(self):
+        """Test the save method of the BaseModel"""
+        model = BaseModel()
+        model.save()
+        self.assertNotEqual(model.updated_at, model.created_at)
+
+    def test_to_dict(self):
+        """Test the to_dict method of the BaseModel"""
+        model = BaseModel()
+        obj_dict = model.to_dict()
+        self.assertIsInstance(obj_dict, dict)
+        self.assertEqual(obj_dict["__class__"], "BaseModel")
+        self.assertEqual(obj_dict["created_at"], model.created_at.isoformat())
+        self.assertEqual(obj_dict["updated_at"], model.updated_at.isoformat())
