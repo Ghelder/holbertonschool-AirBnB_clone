1a9a6a95e24487a4ebf08412f1d437e80e913e7f
Ghelder <Jhelder@outlook.com>
2023-07-07 17:53:47 -0700

    fix: set instance attributes

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,40 @@ from datetime import datetime
 class BaseModel:
     """Base class for all AirBnB models"""
 
-    def __init__(self):
-        """Initializes a new instance of the BaseModel class."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, **kwargs):
+        """Initializes a BaseModel instance.
+
+        Args:
+            kwargs (dict): Optional keyword arguments to set instance
+            attributes.
+        """
+        if kwargs:
+            self.set_attributes_from_kwargs(kwargs)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def set_attributes_from_kwargs(self, kwargs):
+        """Sets instance attributes from a dictionary.
+
+        Args:
+            kwargs (dict): Dictionary containing attribute-value pairs.
+        """
+        for key, value in kwargs.items():
+            # if key == "created_at" or key == "updated_at":
+            if key in ("created_at", "updated_at"):
+                setattr(self, key, self.parse_datetime(value))
+            elif key != "__class__":
+                setattr(self, key, value)
+
+    def parse_datetime(self, value):
+        """Sets instance attributes from a dictionary.
+
+        Args:
+            kwargs (dict): Dictionary containing attribute-value pairs.
+        """
+        return datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
 
     def __str__(self):
         """Returns a string representation of the BaseModel object."""
