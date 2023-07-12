24d2050604aab768bf303d7f058c67d8cb29bde9
Ghelder <Jhelder@outlook.com>
2023-07-12 16:30:10 -0700

    test: Add validation for None

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -10,11 +10,13 @@ class TestCity(unittest.TestCase):
     def test_city_state_id(self):
         """Test the state_id property of the City class."""
         city = City()
-        city.state_id = "CA"
-        self.assertEqual(city.state_id, "CA")
+        city.state_id = ["CA", None]
+        self.assertEqual(city.state_id[0], "CA")
+        self.assertIsNone(city.state_id[1])
 
     def test_city_name(self):
         """Test the name property of the City class."""
         city = City()
-        city.name = "San Francisco"
-        self.assertEqual(city.name, "San Francisco")
+        city.name = ["San Francisco", None]
+        self.assertEqual(city.name[0], "San Francisco")
+        self.assertIsNone(city.name[1])
